7c845a656db38184fb3dec9cdac39a1634082390
Rob Baily <rob.baily@leidos.com>
2023-03-07 09:15:43 -0500

    Change conditions

--- deb-s3-upload/determine_codename.sh
+++ deb-s3-upload/determine_codename.sh
@@ -5,10 +5,12 @@ echo "GITHUB_REF=$GITHUB_REF"
 echo "GITHUB_REF_NAME=$GITHUB_REF_NAME"
 
 # check for specific branches
-if [[ ${GITHUB_REF} =~ feature/.* ]]; then
-    APT_CODENAME=feature-${GITHUB_REF_NAME}
-elif [[ ${GITHUB_REF} =~ candidate/.* ]]; then
-    APT_CODENAME=candidate-${GITHUB_REF_NAME}
+if [[ ${GITHUB_REF_NAME} =~ feature/.* ]]; then
+    APT_CODENAME=feature-${GITHUB_REF##*/}
+elif [[ ${GITHUB_REF_NAME} =~ candidate/.* ]]; then
+    APT_CODENAME=candidate-${GITHUB_REF##*/}
+elif [[ ${GITHUB_REF_NAME} =~ release/.* ]]; then
+    APT_CODENAME=release-${GITHUB_REF##*/}
 elif [[ ${GITHUB_REF_PROTECTED} = true ]]; then
     APT_CODENAME=develop
 else
