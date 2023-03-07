d63f5fd974b52b6c44f6eece4b45a4e01e3b266a
Rob Baily <rob.baily@leidos.com>
2023-03-07 09:44:05 -0500

    Use calculated codename in upload

--- deb-s3-upload/determine_codename.sh
+++ deb-s3-upload/determine_codename.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "GITHUB_REF_PROTECTED=$GITHUB_REF_PROTECTED"
-echo "GITHUB_REF=$GITHUB_REF"
-echo "GITHUB_REF_NAME=$GITHUB_REF_NAME"
-
 NO_UNDERSCORE_NAME=${GITHUB_REF_NAME/_/-}
 # check for specific branches
 if [[ ${GITHUB_REF_NAME} =~ feature/.* ]]; then
@@ -13,8 +9,10 @@ elif [[ ${GITHUB_REF_NAME} =~ candidate/.* ]]; then
 elif [[ ${GITHUB_REF_NAME} =~ release/.* ]]; then
     APT_CODENAME=release-${NO_UNDERSCORE_NAME##*/}
 elif [[ ${GITHUB_REF_PROTECTED} == true ]]; then
+    # if this is a protected branch then assume this is develop
     APT_CODENAME=develop
 else
+    # this is the default if this is just a branch that is being pushed for PR
     APT_CODENAME=ci-build
 fi
 
