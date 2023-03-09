da7711cecb3c63655cf1bc3944781fcf43d78522
Rob Baily <rob-baily@users.noreply.github.com>
2023-03-09 09:23:57 -0500

    Repo codename setup (#17)
    * Set up script to generate repo codename based on branch name
    
    * Set up test action to compile and upload artifacts
    
    * Change Deb S3 docker build to only trigger when appropriate files are modified.

---
+++ deb-s3-upload/determine_codename.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# use dashes instead of underscores in our repo code names
+NO_UNDERSCORE_NAME=${GITHUB_REF_NAME/_/-}
+DEVELOP_BRANCHES="carma-develop develop"
+
+# check for feature branches
+if [[ ${GITHUB_REF_NAME} =~ feature/.* ]]; then
+    APT_CODENAME=feature-${NO_UNDERSCORE_NAME##*/}
+
+# check for release candidate branches
+elif [[ ${GITHUB_REF_NAME} =~ release/.* ]]; then
+    APT_CODENAME=candidate-${NO_UNDERSCORE_NAME##*/}
+
+# check for release tags
+elif [[ $GITHUB_REF_TYPE = tag ]]; then
+    APT_CODENAME=release-${NO_UNDERSCORE_NAME##*/}
+
+# check for develop branches
+elif [[ $DEVELOP_BRANCHES =~ (^|[[:space:]])${GITHUB_REF_NAME}($|[[:space:]]) ]]; then
+    APT_CODENAME=develop
+
+# this is the default if this is just a branch that is being pushed for PR
+else
+    APT_CODENAME=ci-build
+fi
+
+echo ${APT_CODENAME}
---
+++ tests/cpp_project/README.md
@@ -0,0 +1,1 @@
+Required file for Debian packaging
